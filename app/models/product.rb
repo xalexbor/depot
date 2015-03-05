adf96339caba750072fddc13f498f1b0e002a72d
Alex Bor <xalexbor@gmail.com>
2015-03-05 15:10:33 +0300

    Add Validates

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ActiveRecord::Base
+  validates :name,:description,:image_url,presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+  validates :name, uniqueness: true
 end
