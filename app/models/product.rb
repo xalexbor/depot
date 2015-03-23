079f0a2a5ecc7db2d54a78a16c6d63279a058430
Alex Bor <xalexbor@gmail.com>
2015-03-23 11:41:00 +0300

    Create Cart and Line Items

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,18 @@ class Product < ActiveRecord::Base
   validates :name,:description,:image_url,presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :name, uniqueness: true
+
+  has_many :line_items
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+
+  private
+    def ensure_not_referenced_by_any_line_item
+        if line_items.epmty?
+          return true
+        else
+          errors.add(:base, "Products exists")
+          return false
+        end
+    end
 end
